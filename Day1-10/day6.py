5e06096af7fe0a5bd15eaf9bbca6603518fcd6a9
Chris Kusha <chriskusha@yahoo.com>
2023-02-11 11:20:57 -0500

    day6

---
+++ Day1-10/day6.py
@@ -0,0 +1,21 @@
+# Day 6 - Reeborg maze bot
+# for use on https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+# to prevent cases where the robot circles around indefinitely
+while front_is_clear():
+    move()
+left_turn()
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
