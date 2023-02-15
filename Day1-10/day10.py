c91bb727ddcd5b4ed2e52a109fa347f1bdad74ae
Chris Kusha <chriskusha@yahoo.com>
2023-02-15 11:27:48 -0500

    day10

---
+++ Day1-10/day10.py
@@ -0,0 +1,50 @@
+# Day 10 - Calculator
+
+from day10art import logo
+
+# Calculator
+
+# Add
+def add(n1, n2):
+    return n1 + n2
+
+# Subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+# Multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+# Divide
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    '+' : add,
+    '-' : subtract,
+    '*' : multiply,
+    '/' : divide
+}
+
+def calculator():
+    finished = False
+    print(logo)
+    num1 = float(input('What\'s the first number?: '))
+    for symbol in operations:
+        print(symbol)
+
+    while not finished:
+        operation_symbol = input('Pick an operation: ')
+        num2 = float(input('What\'s the next number?: '))
+        answer = operations[operation_symbol](num1, num2)
+
+        print(f'{num1} {operation_symbol} {num2} = {answer}')
+        calc_more = input(f"Type 'y' to continue calculating with {answer} or type 'n' to start a new calculation.: ")
+        if calc_more == 'n':
+            finished = True
+            calculator()
+        else:
+            num1 = answer
+
+calculator()
